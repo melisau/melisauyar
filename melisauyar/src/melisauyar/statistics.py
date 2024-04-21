b6a72056aefd8f175966d28243f133a558d977c2
melisau <77450361+melisau@users.noreply.github.com>
2024-04-21 23:39:49 +0300

    Delete melisauyar directory

--- melisauyar/src/melisauyar/statistics.py
+++
@@ -1,5 +0,0 @@
-
-def mean(x:list)-> float:
-    """" Calculate the mean of a list of numbers."""
-    return sum(x) / len(x)
-    
--- melisauyar/tests/test_statistics.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append(".")
-import pytest
-from src.melisauyar import statistics
-
-def test_mean():
-    assert statistics.mean([1,2,3,4,5]) == 3
